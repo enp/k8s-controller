5f5f026d87e0a1b0c7a1b061e872fe337aa50df0
Eugene Prokopiev <enp@itx.ru>
2021-04-14 16:34:22 +0300

    minimal web controller implementation

--- src/main/groovy/ru/itx/enp/k8s/WebController.groovy
+++ src/main/groovy/ru/itx/enp/k8s/WebController.groovy
@@ -1,5 +1,7 @@
 package ru.itx.enp.k8s
 
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpServer
 import groovy.transform.TupleConstructor
 import groovy.util.logging.Slf4j
 
@@ -7,10 +9,22 @@ import groovy.util.logging.Slf4j
 @TupleConstructor()
 class WebController {
 	JobController jobController
+	private String httpPort = System.getenv('HTTP_PORT') ?: '8080'
+	private HttpServer httpServer = HttpServer.create(new InetSocketAddress(httpPort.toInteger()), 0)
 	void start() {
 		log.info('start')
+		httpServer.createContext("/") { HttpExchange httpExchange ->
+			log.info("request from ${httpExchange.remoteAddress}")
+			httpExchange.responseHeaders.add("Content-type", "text/plain")
+			httpExchange.sendResponseHeaders(200, 0)
+			httpExchange.responseBody.withWriter { out ->
+				out << "HELLO\n"
+			}
+		}
+		httpServer.start()
 	}
 	void stop() {
+		httpServer.stop(0)
 		log.info('stop')
 	}
 }
